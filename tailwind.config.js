96242c9c23d5830c147e96559d81c9ca0dbacb82
Shola Ayeni <ayenisholah@yahoo.com>
2022-02-17 16:24:27 +0100

    tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,44 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      mobile: '400px',
+      tablet: '768px',
+      laptop: '1120px',
+      desktop: '1360px',
+    },
+    fontWeight: {
+      bold: 700,
+    },
+    fontSize: {
+      sm: [
+        '15px',
+        {
+          lineHeight: '20px',
+        },
+      ],
+    },
+
+    extend: {
+      colors: {
+        body: '#FFFFFF',
+        theme: '#389D3C',
+        white: '#FFFFFF',
+        'frog-nation-gray': '#676C67',
+        // nav: '#FFFFFF',
+        // secondary: '',
+        // badge: '',
+        // input: '',
+        // 'input-border': '',
+        'selected-text': '#389D3C',
+      },
+      fontFamily: {
+        nunito: ['Nunito Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-};
+}
